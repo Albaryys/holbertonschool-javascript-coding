00914e523e9649206ed489f52b28f7925418c5e0
Albarys_ <a.tily.pro@gmail.com>
2023-09-19 12:16:53 +0200

    correct task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
+// process.argv[2]' est le 3eme arg : contient le chemin du fichier à lire.
 const fs = require('fs');
-
-// Check if the user provided a file path as the first argument
-if (process.argv.length !== 3) {
-  console.error('Usage: ./0-readme.js <file-path>');
-  process.exit(1);
-}
-
-// Read the content of the file in utf-8 encoding
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
