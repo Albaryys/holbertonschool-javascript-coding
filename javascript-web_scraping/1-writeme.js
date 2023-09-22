7702582b6e17aeb9039d69d0f34d76078a676170
Albarys_ <a.tily.pro@gmail.com>
2023-09-22 10:15:23 +0200

    add task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,22 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-// Vérification des arguments de la ligne de commande
-if (process.argv.length !== 4) {
-  console.error('Utilisation : ./1-writeme.js <chemin-du-fichier> "<chaine-a-ecrire>"');
-  process.exit(1);
-}
-
-// Récupération du chemin du fichier et de la chaîne à écrire
 const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
+const content = process.argv[3];
 
-// Écriture de la chaîne dans le fichier en UTF-8
-fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
-  if (err) {
-    // En cas d'erreur, affichage de l'objet d'erreur
-    console.error(err);
-  } else {
-    console.log('Le contenu a été écrit avec succès dans le fichier.');
+fs.writeFile(filePath, content, 'utf-8', (error) => {
+  if (error) {
+    console.error(error);
   }
 });
